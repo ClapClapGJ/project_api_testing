8757c0ccca0797a921412f6d9262d6666c45ab29
Artem <fantazerbeda@gmail.com>
2022-11-22 19:52:30 +0400

    create api testing project

---
+++ tests/test_user_auth.py
@@ -0,0 +1,81 @@
+import pytest
+from Lib.base_case import BaseCase
+from Lib.my_requests import MyRequests
+from Lib.assertions import Assertions
+import allure
+
+
+@allure.epic("Authorization cases")
+class TestUserAuth(BaseCase):
+    """параметры для негативного кейса"""
+    exclude_params = [
+        'no_cookie',
+        'no_token'
+    ]
+
+    def setup(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+
+        """авторизируем пользователя"""
+        response1 = MyRequests.post('/user/login', data=data)
+
+        self.auth_sid = self.get_cookie(response1, 'auth_sid')
+        self.token = self.get_header(response1, 'x-csrf-token')
+        self.user_id_from_auth_method = self.get_json_value(response1, 'user_id')
+
+    @allure.description("This is successfully authorization user by email and password")
+    def test_auth_user(self):
+
+        """проверяем что такой пользователь авторизовался"""
+
+        response2 = MyRequests.get(
+            '/user/auth',
+            headers={'x-csrf-token': self.token},
+            cookies={'auth_sid': self.auth_sid}
+        )
+
+        Assertions.assert_json_value_by_name(
+            response2,
+            'user_id',
+            self.user_id_from_auth_method,
+            'User id from auth method is not equal to user id from check method'
+        )
+
+    @allure.description("This test checks authorization status without sending auth cookie or token")
+    @pytest.mark.parametrize('condition', exclude_params)
+    def test_negative_auth_check(self, condition):
+
+        if condition == 'no_cookie':
+            response2 = MyRequests.get(
+                '/user/auth',
+                headers={'x-csrf-token': self.token}
+            )
+        else:
+            response2 = MyRequests.get(
+                '/user/auth',
+                cookies={'auth_sid': self.auth_sid}
+            )
+
+        Assertions.assert_json_value_by_name(
+            response2,
+            'user_id',
+            0,
+            f'User is authorized with condition {condition}'
+        )
+
+    def test_home_cookie(self):
+
+        response_home_cookie = MyRequests.get(
+            '/homework_cookie',
+            headers={'x-csrf-token': self.token},
+            cookies={'auth_sid': self.auth_sid}
+        )
+        self.HomeWork = self.get_cookie(response_home_cookie, 'HomeWork')
+
+        Assertions.assert_cookie_value_by_name(response_home_cookie,
+                                               'HomeWork',
+                                               self.HomeWork,
+                                               f'There is no such cookie name {self.HomeWork}')
